3174f79c64f017ad611053be5b22ade546d38f98
CTZDev <carlosteranzavaleta@hotmail.com>
2025-01-18 18:00:42 -0500

    chore(app): Change name app to CertiSaludApp

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@fontsource-variable/montserrat";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import CertiSaludApp from "./CertiSaludApp.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CertiSaludApp />
+  </StrictMode>
+);
