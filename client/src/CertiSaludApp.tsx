1638057255d2c0dc5c6a8f2cce1e7413638523ea
CTZDev <carlosteranzavaleta@hotmail.com>
2025-01-17 21:14:01 -0500

    chore(frontend): rename App to CertiSaludApp

---
+++ client/src/CertiSaludApp.tsx
@@ -0,0 +1,8 @@
+import "./CertiSaludApp.css";
+import { LoginPage } from "./features/auth/pages/LoginPage";
+
+function CertiSaludApp() {
+  return <LoginPage />;
+}
+
+export default CertiSaludApp;
