49639d44ebc86f8e84c397222ce01bbda010bf0e
JosmenDev <josmendev@gmail.com>
2025-01-25 23:11:45 -0500

    fix(auth): corrected user-role guard creation by removing unnecessary 'user-role' folder and placing the guard directly in the 'guards' folder

--- server/src/auth/decorators/auth.decorator.ts
+++ server/src/auth/decorators/auth.decorator.ts
@@ -2,7 +2,7 @@ import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { Role } from '../enums/role.enum';
 import { AuthGuard } from '@nestjs/passport';
 import { ROLES_KEY } from './roles.decorator';
-import { UserRoleGuard } from '../guards/user-role/user-role.guard';
+import { UserRoleGuard } from '../guards/user-role.guard';
 
 export function Auth(...roles: Role[]) {
   return applyDecorators(
