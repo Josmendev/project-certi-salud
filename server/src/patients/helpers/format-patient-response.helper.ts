b19f5fbd95583f8db8a6677ce1bea732206d38ec
JosmenDev <josmendev@gmail.com>
2025-01-29 11:07:19 -0500

    feat(patient): implemented method to add a patient, including creation of helper methods and use of queryRunner for multiple transactions

---
+++ server/src/patients/helpers/format-patient-response.helper.ts
@@ -0,0 +1,18 @@
+import { Patient } from '../entities/patient.entity';
+
+export const formatPatientResponse = (patient: Patient) => {
+  const {patientId, age, isActive, person} = patient;
+  const {identityDocumentNumber, name, paternalSurname, maternalSurname, personId} = person;
+  return {
+    patientId,
+    isActive,
+    age,
+    person: {
+      identityDocumentNumber,
+      name,
+      paternalSurname,
+      maternalSurname,
+      personId,
+    },
+  };
+} 
---
+++ server/src/patients/interfaces/patient-response.interface.ts
@@ -0,0 +1,8 @@
+import { PersonResponse } from "src/persons/interfaces/person-response.interface";
+
+export interface PatientResponse {
+  patientId: number;
+  isActive: boolean;
+  age: number;
+  person: PersonResponse;
+}
