f089386bf0d8236462aed147599821643881a79c
JosmenDev <josmendev@gmail.com>
2025-02-01 15:51:03 -0500

    fix(common): removed unused private variable key from api-fetch.adapter

--- server/src/common/adapters/api-fetch.adapter.ts
+++ server/src/common/adapters/api-fetch.adapter.ts
@@ -4,19 +4,22 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ApiFetchAdapter implements HttpAdapter {
-  private apiKey: string;
   constructor(
     private readonly configService: ConfigService
-  ){
-    this.apiKey = this.configService.get<string>('API_KEY_RENIEC');
-  }
+  ){}
 
-  async get<T>(url: string): Promise<T> {
-    const res = await fetch(url, {
-      headers: {'Authorization' : `Bearer ${this.apiKey}`}
-    });
+  async get<T>(url: string, apiKey?: string): Promise<T> {
+    const key = apiKey || this.getApiKeyForUrl(url);
+    const headers: HeadersInit = {};
+    if(key) headers['Authorization'] = `Bearer ${key}`;
+    const res = await fetch(url, headers);
     if (!res.ok) throw new HttpException('Error al intentar conectar con la API externa', HttpStatus.INTERNAL_SERVER_ERROR)
     const data: T = await res.json();
     return data;
+  }
+
+  private getApiKeyForUrl (url: string) : string {
+    if(url.includes('https://apiperu.dev/api/dni')) return this.configService.get<string>('API_KEY_RENIEC');
+    return null;
   }
 }
