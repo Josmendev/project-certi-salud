8732e173dff6b5bc5bddae5ddc90feb8b45c1f44
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-12 11:54:22 -0500

    feat (utils): Add constants in the app that are used for the all moduls

---
+++ client/src/shared/utils/constants.ts
@@ -0,0 +1,23 @@
+export const BASE_BACKEND_URL = "http://localhost:3002/api/v1";
+
+export const BASE_ROUTES = {
+  PUBLIC: {
+    LOGIN: "/login",
+    CONFIRM_ACCOUNT: "confirm-account",
+    HOME: "/",
+  },
+  PRIVATE: {
+    DASHBOARD: "dashboard",
+    ADMIN: "admin",
+    INFO_REQUIRED: "info-required",
+    REGISTER_CERTIFICATE: "register-certificate",
+    REPORTS: "reports",
+  },
+};
+
+export const CRUD_ROUTES = {
+  LIST: "list",
+  ADD: "add",
+  DETAIL: ":id",
+  EDIT: ":id/edit",
+};
