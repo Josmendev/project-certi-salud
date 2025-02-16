1670c344c0dc304ade8c34e9dd8404e9628c7881
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-15 19:32:31 -0500

    chore (component): Remove form for the search and config with keypress

--- client/src/shared/components/Search.tsx
+++ client/src/shared/components/Search.tsx
@@ -17,31 +17,27 @@ export const Search: React.FC<Props> = ({
 }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSearch(searchQuery);
-  };
-
   return (
-    <form className="w-full bg-shades-light" onSubmit={handleSearch}>
-      <div className="w-80 -mt-3 mb-5 relative ml-auto">
-        <input
-          type="text"
-          id={id}
-          name={name}
-          className="w-full bg-shades-light border border-neutral-100 rounded-md p-2.5"
-          placeholder={placeholder}
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <Button
-          title="Buscar"
-          id="btnSearchUser"
-          type="submit"
-          classButton="pointer-events-none absolute top-1/2 right-0 -translate-y-1/2 px-3.5"
-          iconRight={<Icon.Search color="#9ca3af" size={20} />}
-        />
-      </div>
-    </form>
+    <div className="w-80 -mt-3 mb-5 relative ml-auto">
+      <input
+        type="text"
+        id={id}
+        name={name}
+        className="w-full bg-shades-light border border-neutral-100 rounded-md p-2.5"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+          onSearch(e.target.value);
+        }}
+      />
+      <Button
+        title="Buscar"
+        id="btnSearchUser"
+        type="submit"
+        classButton="pointer-events-none absolute top-1/2 right-0 -translate-y-1/2 px-3.5"
+        iconRight={<Icon.Search color="#9ca3af" size={20} />}
+      />
+    </div>
   );
 };
