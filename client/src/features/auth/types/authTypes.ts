bbfcede11653c0fae2587b52623fefdfa7c53154
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-15 19:31:05 -0500

    chore (type): Update type of TokenLogoutUser

--- client/src/features/auth/types/authTypes.ts
+++ client/src/features/auth/types/authTypes.ts
@@ -1,14 +1,17 @@
 import type { IUser } from "./User";
 
 // Defino los tipos de INPUT para el modulo de Auth
-export type AuthLoginUser = Pick<IUser, "username" | "password">;
+export type AuthLoginUser = {
+  username: string;
+  password: string;
+};
+
 export type AuthConfirmUser = {
   newPassword: string;
   repeatPassword: string;
 };
-export type AuthLogoutUser = Pick<IUser, "token">;
+
+export type TokenLogoutUser = string;
 
 // Defino el tipo de OUTPUT para el modulo de Auth
-export type AuthResponseUser = Pick<IUser, "userId" | "username" | "token" | "isConfirm"> & {
-  isActive?: boolean;
-};
+export type AuthResponseUser = IUser & { token: string };
