16740308defd42ea75b39b0ee0952f9f975ba1cc
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-21 16:01:18 -0500

    feat(admin-users): Refactor ListOfUsersService to improve error handling and parameter structure

--- client/src/features/admin-users/users/services/ListOfUsersService.ts
+++ client/src/features/admin-users/users/services/ListOfUsersService.ts
@@ -1,15 +1,17 @@
 import type { DataResponseFromAPI } from "../../../../shared/types/DataResponse";
 import { INITIAL_PAGE, LIMIT_PAGE } from "../../../../shared/utils/constants";
+import { parseErrorResponse } from "../../../../shared/utils/parseErrorResponse";
 import type { DataOfUser } from "../types/userTypes";
 import { ENDPOINTS_USER } from "../utils/endpoints";
-import { ErrorResponse } from "./../../../../shared/types/ErrorResponse";
-import { handleError } from "./../../../../shared/utils/handleError";
 
-// Creo la funcion login que se conecta a la API del backend
-export const ListOfUsersService = async (
-  limit: number = LIMIT_PAGE,
-  page: number = INITIAL_PAGE
-): Promise<DataResponseFromAPI<DataOfUser> | ErrorResponse> => {
+// Creo la funcion listOfUsers que se conecta a la API del backend
+export const ListOfUsersService = async ({
+  limit = LIMIT_PAGE,
+  page = INITIAL_PAGE,
+}: {
+  limit?: number;
+  page: number;
+}): Promise<DataResponseFromAPI<DataOfUser>> => {
   try {
     const { token } = JSON.parse(sessionStorage.getItem("user") as string);
     if (!token) throw new Error("Token inválido");
@@ -22,16 +24,13 @@ export const ListOfUsersService = async (
       },
     });
 
-    // Respuesta no exitosa, lanzo excepcion con (message, status, details)
-    if (!response.ok) {
-      const errorResponse = await response.json();
-      return handleError(errorResponse);
-    }
+    // Respuesta no exitosa, lanzo excepcion del backend
+    if (!response.ok) throw await response.json();
 
-    // Respuesta exitosa, parseo el JSON y devuelvo el objeto AuthResponseUser
+    // Respuesta exitosa, parseo el JSON y devuelvo el objeto DataResponseFromAPI<DataOfUser>
     const data: DataResponseFromAPI<DataOfUser> = await response.json();
     return data;
   } catch (error: unknown) {
-    return handleError(error);
+    throw parseErrorResponse(error);
   }
 };
