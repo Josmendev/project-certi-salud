0cf7f856b91a063119b1f554581782326e97e142
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-20 21:49:09 -0500

    feat(auth): Add checkTokenExpiration utility to manage token expiration checks

---
+++ client/src/shared/utils/authCheckToken.ts
@@ -0,0 +1,17 @@
+import { jwtDecode } from "jwt-decode";
+
+export const checkTokenExpiration = (token: string): Promise<string> => {
+  return new Promise((resolve) => {
+    try {
+      const decoded = jwtDecode<{ exp: number }>(token);
+      const expirationTime = decoded?.exp * 1000; // conversión a milisegundos
+      const now = Date.now() + 2000;
+
+      const timer = setTimeout(() => resolve("expired"), expirationTime - now);
+      if (expirationTime <= now) clearTimeout(timer);
+    } catch (error) {
+      console.log(error);
+      resolve("expired");
+    }
+  });
+};
