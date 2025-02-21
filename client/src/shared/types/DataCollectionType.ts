b42a482b0c937f9ac6a882a7113c327f3decc138
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-20 21:49:01 -0500

    refactor(types): Update DataCollectionType to use DataOfUser for consistency

--- client/src/shared/types/DataCollectionType.ts
+++ client/src/shared/types/DataCollectionType.ts
@@ -1,4 +1,4 @@
-import type { DataOfUsers } from "../../features/admin-users/users/types/userTypes";
+import type { DataOfUser } from "../../features/admin-users/users/types/userTypes";
 
 // Creo todos los tipos de datos a asignar en tablas
-export type DataCollectionType = DataOfUsers;
+export type DataCollectionType = DataOfUser;
