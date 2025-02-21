2ebc08ce66abbbecf54231fec81a2c6c5c18d848
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-21 16:02:38 -0500

    feat(service): Refactor LogoutService for improved error handling and response parsing

--- client/src/features/auth/services/LogoutService.ts
+++ client/src/features/auth/services/LogoutService.ts
@@ -1,10 +1,9 @@
-import { ErrorResponse } from "../../../shared/types/ErrorResponse";
 import { ENDPOINTS_AUTH } from "../../../shared/utils/endpoints";
-import { handleError } from "../../../shared/utils/handleError";
+import { parseErrorResponse } from "../../../shared/utils/parseErrorResponse";
 import { type TokenLogoutUser } from "../types/authTypes";
 
 // Creo la funcion logout que se conecta a la API del backend
-export const LogoutService = async (token: TokenLogoutUser): Promise<void | ErrorResponse> => {
+export const LogoutService = async (token: TokenLogoutUser): Promise<void> => {
   try {
     const response = await fetch(`${ENDPOINTS_AUTH.LOGOUT}`, {
       method: "POST",
@@ -15,11 +14,8 @@ export const LogoutService = async (token: TokenLogoutUser): Promise<void | Erro
     });
 
     // Respuesta no exitosa, lanzo excepcion del backend
-    if (!response.ok) {
-      const errorResponse = await response.json();
-      return handleError(errorResponse);
-    }
+    if (!response.ok) throw await response.json();
   } catch (error: unknown) {
-    return handleError(error);
+    throw parseErrorResponse(error);
   }
 };
