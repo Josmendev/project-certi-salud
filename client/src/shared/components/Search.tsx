9f20b923e0b388230193778cbd38f17ed3004288
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-23 10:57:33 -0500

    refactor(Search): add className prop for customizable styling

--- client/src/shared/components/Search.tsx
+++ client/src/shared/components/Search.tsx
@@ -6,6 +6,7 @@ interface Props {
   id: string;
   name: string;
   placeholder?: string;
+  className?: string;
   onSearch?: (query: string) => void;
 }
 
@@ -13,12 +14,13 @@ export const Search: React.FC<Props> = ({
   id,
   name,
   placeholder = "Buscar ...",
+  className = "",
   onSearch = () => {},
 }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <div className="w-80 -mt-3 mb-5 relative ml-auto">
+    <div className={`w-80 -mt-3 mb-5 relative ml-auto ${className}`}>
       <input
         type="text"
         id={id}
