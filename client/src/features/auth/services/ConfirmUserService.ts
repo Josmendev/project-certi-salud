f27cea31238a4c0be72c4f07590a04f222441145
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-23 10:56:20 -0500

    refactor(ConfirmUserService): replace parseErrorResponse with handleApiError for improved error handling

--- client/src/features/auth/services/ConfirmUserService.ts
+++ client/src/features/auth/services/ConfirmUserService.ts
@@ -1,5 +1,5 @@
 import { ENDPOINTS_AUTH } from "../../../shared/utils/endpoints";
-import { parseErrorResponse } from "../../../shared/utils/parseErrorResponse";
+import { handleApiError } from "../../../shared/utils/handleApiError";
 import { type AuthConfirmUser, type AuthResponseUser } from "../types/authTypes";
 
 // Creo la funcion confirmUser que se conecta a la API del backend
@@ -23,6 +23,7 @@ export const ConfirmUserService = async (
     const data: AuthResponseUser = await response.json();
     return data;
   } catch (error: unknown) {
-    throw parseErrorResponse(error);
+    handleApiError(error);
+    return Promise.reject(error); // Similar al throw, pero enfocado a promesas
   }
 };
