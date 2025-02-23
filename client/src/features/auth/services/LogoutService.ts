88d24da2b1fea15f1d2f7136bbc9f01b912b3790
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-23 10:56:51 -0500

    refactor(LogoutService): replace parseErrorResponse with handleApiError for improved error handling

--- client/src/features/auth/services/LogoutService.ts
+++ client/src/features/auth/services/LogoutService.ts
@@ -1,5 +1,5 @@
 import { ENDPOINTS_AUTH } from "../../../shared/utils/endpoints";
-import { parseErrorResponse } from "../../../shared/utils/parseErrorResponse";
+import { handleApiError } from "../../../shared/utils/handleApiError";
 import { type TokenLogoutUser } from "../types/authTypes";
 
 // Creo la funcion logout que se conecta a la API del backend
@@ -16,6 +16,7 @@ export const LogoutService = async (token: TokenLogoutUser): Promise<void> => {
     // Respuesta no exitosa, lanzo excepcion del backend
     if (!response.ok) throw await response.json();
   } catch (error: unknown) {
-    throw parseErrorResponse(error);
+    handleApiError(error);
+    return Promise.reject(error); // Similar al throw, pero enfocado a promesas
   }
 };
