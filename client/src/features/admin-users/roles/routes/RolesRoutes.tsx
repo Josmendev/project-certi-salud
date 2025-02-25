0dc0ec70adae3b01967d88cea000dfd01bd59d8d
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-24 23:33:23 -0500

    refactor(route): remove unused role pages from routing

--- client/src/features/admin-users/roles/routes/RolesRoutes.tsx
+++ client/src/features/admin-users/roles/routes/RolesRoutes.tsx
@@ -1,9 +1,6 @@
 import type React from "react";
 import { Navigate, Route, Routes } from "react-router";
 import { CRUD_ROUTES } from "../../../../shared/utils/constants";
-import { RoleCreatePage } from "../pages/RoleCreatePage";
-import { RoleDetailPage } from "../pages/RoleDetailPage";
-import { RoleEditPage } from "../pages/RoleEditPage";
 import { RoleListPage } from "../pages/RoleListPage";
 
 export const RoleRoutes: React.FC = () => {
@@ -12,9 +9,6 @@ export const RoleRoutes: React.FC = () => {
       <Routes>
         <Route index element={<RoleListPage />} />
         <Route path={CRUD_ROUTES.LIST} element={<RoleListPage />} />
-        <Route path={CRUD_ROUTES.ADD} element={<RoleCreatePage />} />
-        <Route path={CRUD_ROUTES.DETAIL} element={<RoleDetailPage />} />
-        <Route path={CRUD_ROUTES.EDIT} element={<RoleEditPage />} />
         <Route path="*" element={<Navigate to="." replace />} />
       </Routes>
     </>
