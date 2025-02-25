961c7cec3d578d580af8c9c6d50be0ce5a502651
CTZDev <carlosteranzavaleta@hotmail.com>
2025-02-24 23:34:32 -0500

    refactor(staff): update Staff interface and add response types for improved clarity

--- client/src/features/admin-users/staff/types/Staff.ts
+++ client/src/features/admin-users/staff/types/Staff.ts
@@ -1,5 +1,17 @@
-export interface IStaff {
-  staff_id: number;
-  person_id: number;
-  isActive: boolean;
+import type { Person } from "../../../../shared/types/Person";
+
+export interface Staff {
+  identityDocumentNumber: number;
+  name: number;
+  paternalSurname: boolean;
+  maternalSurname: boolean;
 }
+
+export interface StaffResponse {
+  staffId: number;
+  isActive: boolean;
+  person: Person;
+}
+
+export type StaffUpdateRequest = Partial<Staff>;
+export type StaffAssignRequest = Pick<Staff, "identityDocumentNumber">;
