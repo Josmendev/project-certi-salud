6f8ce219153278d78a7b744e4a291bef56a63c9c
JosmenDev <josmendev@gmail.com>
2025-02-25 23:18:39 -0500

    feat(database): initial configuration of migrations in NestJS

---
+++ server/src/database/migrations/1700000000000-CreateGenerateCertificateCodeProcedure.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreatedGenerateCertificateCode1700000000000
+  implements MigrationInterface
+{
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE PROCEDURE GenerateCertificateCode()
+      BEGIN
+        SELECT 
+          CASE 
+            WHEN COUNT(*) > 0 THEN 
+              CONCAT('C-', YEAR(NOW()), MONTH(NOW()), LPAD(SUBSTR(MAX(certificate_code), 9, 6) + 1, 5, '0'))
+            ELSE 
+              CONCAT('C-', YEAR(NOW()), MONTH(NOW()), '00001')
+          END AS certificate_code
+        FROM 
+          certificate
+        WHERE 
+          YEAR(issue_date) = YEAR(NOW()) 
+          AND MONTH(issue_date) = MONTH(NOW());
+      END;
+    `);
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP PROCEDURE IF EXISTS GenerateCertificateCode;`,
+    );
+  }
+}
