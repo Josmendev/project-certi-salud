ed973d176450eb9eeebc5b44d175a71fe7e0e3b1
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-19 22:02:25 -0500

    feat(helper): update getUserInformation to use User type instead of DataOfUser

--- client/src/shared/helpers/getUserInformation.ts
+++ client/src/shared/helpers/getUserInformation.ts
@@ -1,7 +1,7 @@
-import type { DataOfUser } from "../../features/admin-users/users/types/userTypes";
 import type { AuthUserResponse } from "../../features/auth/types/authTypes";
+import type { User } from "../../features/auth/types/User";
 
-export const getUserInformation = (user: AuthUserResponse | DataOfUser) => {
+export const getUserInformation = (user: AuthUserResponse | User) => {
   const name = user?.person?.name || "Nombre no disponible";
   const paternalSurname = user?.person?.paternalSurname || "";
   const maternalSurname = user?.person?.maternalSurname || "";
