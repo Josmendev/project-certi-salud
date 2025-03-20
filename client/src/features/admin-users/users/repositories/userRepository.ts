667de7559c36421cd6657d6f2dc96654e5490f89
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-19 21:59:43 -0500

    refactor(users): streamline user retrieval functions and update return types for improved consistency

--- client/src/features/admin-users/users/repositories/userRepository.ts
+++ client/src/features/admin-users/users/repositories/userRepository.ts
@@ -1,38 +1,24 @@
-import type { DataResponseFromAPI } from "../../../../shared/types/DataResponse";
-import { INITIAL_PAGE, LIMIT_PAGE } from "../../../../shared/utils/constants";
+import type { User } from "../../../auth/types/User";
 import { ListOfUsersService } from "../services/ListOfUsersService";
 import { SearchUsersService } from "../services/SearchUsersService";
 import { UpdateUserService } from "../services/UpdateUserService";
-import type { DataOfUser, EditUser } from "../types/userTypes";
+import type { EditUser } from "../types/userTypes";
 
-// Funcion para obtener todos los usuarios
+// Funcion para obtener y buscar todos los users
 export const getUsers = async ({
-  limit = LIMIT_PAGE,
-  page = INITIAL_PAGE,
-}: {
-  limit?: number;
-  page: number;
-}): Promise<DataResponseFromAPI<DataOfUser>> => {
-  const users = await ListOfUsersService({ limit, page });
-  return users;
-};
-
-// Funcion para buscar usuarios
-export const searchUsers = async ({
-  limit = LIMIT_PAGE,
-  page = INITIAL_PAGE,
+  limit,
+  page,
   query = "",
 }: {
   limit?: number;
   page: number;
-  query: string;
-}): Promise<DataResponseFromAPI<DataOfUser>> => {
-  const searchUsers = await SearchUsersService({ limit, page, query });
-  return searchUsers;
+  query?: string;
+}) => {
+  return query ? SearchUsersService({ limit, page, query }) : ListOfUsersService({ limit, page });
 };
 
 // Funcion para editar un usuario
-export const updateUser = async (user: EditUser): Promise<DataOfUser> => {
+export const updateUser = async (user: EditUser): Promise<User> => {
   const updateUser = await UpdateUserService(user);
   return updateUser;
 };
