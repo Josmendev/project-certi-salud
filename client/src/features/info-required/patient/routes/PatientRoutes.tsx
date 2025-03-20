7a159c7e54a9159a35d89d5e4c6ae76826edc724
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-20 18:49:08 -0500

    feat(patient): update PatientRoutes to use PatientUpsertPage for add and edit routes

--- client/src/features/info-required/patient/routes/PatientRoutes.tsx
+++ client/src/features/info-required/patient/routes/PatientRoutes.tsx
@@ -1,9 +1,7 @@
 import { Navigate, Route, Routes } from "react-router";
 import { CRUD_ROUTES } from "../../../../shared/utils/constants";
-import { PatientCreatePage } from "../pages/PatientCreatePage";
-import { PatientDetailPage } from "../pages/PatientDetailPage";
-import { PatientEditPage } from "../pages/PatientEditPage";
 import { PatientListPage } from "../pages/PatientListPage";
+import { PatientUpsertPage } from "../pages/PatientUpsertPage";
 
 export const PatientRoutes: React.FC = () => {
   return (
@@ -11,9 +9,8 @@ export const PatientRoutes: React.FC = () => {
       <Routes>
         <Route index element={<PatientListPage />} />
         <Route path={CRUD_ROUTES.LIST} element={<PatientListPage />} />
-        <Route path={CRUD_ROUTES.ADD} element={<PatientCreatePage />} />
-        <Route path={CRUD_ROUTES.DETAIL} element={<PatientDetailPage />} />
-        <Route path={CRUD_ROUTES.EDIT} element={<PatientEditPage />} />
+        <Route path={CRUD_ROUTES.ADD} element={<PatientUpsertPage />} />
+        <Route path={CRUD_ROUTES.EDIT} element={<PatientUpsertPage />} />
         <Route path="*" element={<Navigate to="." replace />} />
       </Routes>
     </>
