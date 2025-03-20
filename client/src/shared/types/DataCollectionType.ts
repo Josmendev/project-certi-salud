7ac52d75845e75559ec05decf6efc444250a5c17
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-20 18:51:36 -0500

    feat(types): extend DataCollectionType to include PatientResponse and DiseaseResponse

--- client/src/shared/types/DataCollectionType.ts
+++ client/src/shared/types/DataCollectionType.ts
@@ -1,6 +1,13 @@
 import type { RoleResponse } from "../../features/admin-users/roles/types/Role";
 import type { StaffResponse } from "../../features/admin-users/staff/types/Staff";
 import type { User } from "../../features/auth/types/User";
+import type { DiseaseResponse } from "../../features/info-required/disease/types/Disease";
+import type { PatientResponse } from "../../features/info-required/patient/types/Patient";
 
 // Creo todos los tipos de datos a asignar en tablas
-export type DataCollectionType = User | RoleResponse | StaffResponse;
+export type DataCollectionType =
+  | User
+  | RoleResponse
+  | StaffResponse
+  | PatientResponse
+  | DiseaseResponse;
