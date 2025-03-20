02a9272cb8163a0cd8ed2af0fbc5772fa9989d05
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-20 18:47:13 -0500

    feat(disease): add ListDiseaseResponseService for fetching paginated disease data

---
+++ client/src/features/info-required/disease/services/ListDiseasesService.ts
@@ -0,0 +1,37 @@
+import type { DataResponseFromAPI } from "../../../../shared/types/DataResponse";
+import { INITIAL_PAGE, LIMIT_PAGE } from "../../../../shared/utils/constants";
+import { handleApiError } from "../../../../shared/utils/handleApiError";
+import type { DiseaseResponse } from "../types/Disease";
+import { ENDPOINT_DISEASE } from "../utils/endpoints";
+
+// Creo la funcion listDiseases que se conecta a la API del backend
+export const ListDiseaseResponseService = async ({
+  limit = LIMIT_PAGE,
+  page = INITIAL_PAGE,
+}: {
+  limit?: number;
+  page: number;
+}): Promise<DataResponseFromAPI<DiseaseResponse>> => {
+  try {
+    const { token } = JSON.parse(sessionStorage.getItem("user") as string);
+    if (!token) throw new Error("Token inválido");
+
+    const response = await fetch(`${ENDPOINT_DISEASE}?limit=${limit}&page=${page}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    // Respuesta no exitosa, lanzo excepcion del backend
+    if (!response.ok) throw await response.json();
+
+    // Respuesta exitosa, parseo el JSON y devuelvo el objeto DataResponseFromAPI<DiseaseResponse>
+    const data: DataResponseFromAPI<DiseaseResponse> = await response.json();
+    return data;
+  } catch (error: unknown) {
+    handleApiError(error);
+    return Promise.reject(error);
+  }
+};
