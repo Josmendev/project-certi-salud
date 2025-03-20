8e1ca069347faabec6a9071852ac35ecdfa56cff
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-20 18:47:06 -0500

    feat(disease): remove unused routes for disease creation, detail, and editing

--- client/src/features/info-required/disease/routes/DiseaseRoutes.tsx
+++ client/src/features/info-required/disease/routes/DiseaseRoutes.tsx
@@ -1,8 +1,5 @@
 import { Navigate, Route, Routes } from "react-router";
 import { CRUD_ROUTES } from "../../../../shared/utils/constants";
-import { DiseaseCreatePage } from "../pages/DiseaseCreatePage";
-import { DiseaseDetailPage } from "../pages/DiseaseDetailPage";
-import { DiseaseEditPage } from "../pages/DiseaseEditPage";
 import { DiseaseListPage } from "../pages/DiseaseListPage";
 
 export const DiseaseRoutes: React.FC = () => {
@@ -11,9 +8,6 @@ export const DiseaseRoutes: React.FC = () => {
       <Routes>
         <Route index element={<DiseaseListPage />} />
         <Route path={CRUD_ROUTES.LIST} element={<DiseaseListPage />} />
-        <Route path={CRUD_ROUTES.ADD} element={<DiseaseCreatePage />} />
-        <Route path={CRUD_ROUTES.DETAIL} element={<DiseaseDetailPage />} />
-        <Route path={CRUD_ROUTES.EDIT} element={<DiseaseEditPage />} />
         <Route path="*" element={<Navigate to="." replace />} />
       </Routes>
     </>
