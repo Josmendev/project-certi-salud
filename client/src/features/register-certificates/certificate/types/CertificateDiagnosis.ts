e95879ebebef707f0c58f012e22c1aebb2ed6b70
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-20 20:58:17 -0500

    feat(certificates): remove unused Certificate and CertificateDiagnosis interfaces

--- client/src/features/register-certificates/certificate/types/CertificateDiagnosis.ts
+++
@@ -1,5 +0,0 @@
-export interface CertificateDiagnosis {
-  certificate_diagnosis_id: string;
-  disease_id: number;
-  certificate_id: string;
-}
--- client/src/features/register-certificates/certificate/types/TypeCertificate.ts
+++
@@ -1,5 +0,0 @@
-export interface CertificateType {
-  certificate_type_id: number;
-  description: string;
-  isActive: boolean;
-}
