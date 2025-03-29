c0e3458848b5f054adf21a399780beda91110bfb
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-28 20:40:53 -0500

    feat(certificate): enhance Certificate interface and add response types for improved data handling

--- client/src/features/register-certificates/certificate/types/Certificate.ts
+++ client/src/features/register-certificates/certificate/types/Certificate.ts
@@ -1,10 +1,30 @@
-export interface Certificate {
-  certificate_id: string;
-  certificate_code: string;
-  certificate_type_id: number;
-  staff_id: number;
-  patient_id: number;
-  issue_date: string;
-  rest_days: number;
-  status: "Realizado" | "Anulado";
+import type { Person } from "../../../../shared/types/Person";
+import type { DiseaseResponse } from "../../../info-required/disease/types/Disease";
+import type { Patient } from "../../../info-required/patient/types/Patient";
+
+export interface Certificate extends Patient {
+  issueDate: string;
+  certificateTypeId: number;
+  restDays: number;
+  diseases: Array<Pick<DiseaseResponse, "diseaseId">>;
+}
+
+export type PersonByDniResponse = Omit<Person, "personId">;
+
+export interface DiseaseInCertificate {
+  diseaseCie10: string;
+  diseaseDescription: string;
+}
+
+export interface CertificateResponse {
+  certificateId: number;
+  certificateCode: string;
+  certificateTypeDescription: string;
+  staffDni: string;
+  staffName: string;
+  patientDni: string;
+  patientName: string;
+  patientAge: number;
+  restDays: string;
+  diseases: Array<DiseaseInCertificate>;
 }
