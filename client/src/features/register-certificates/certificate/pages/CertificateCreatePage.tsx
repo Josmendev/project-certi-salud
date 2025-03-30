5a846c90de44bde04d0e0d9be297db393282a2d3
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-30 10:57:22 -0500

    refactor(CertificateCreatePage): rename CreateCertificateForm to CertificateForm for consistency

--- client/src/features/register-certificates/certificate/pages/CertificateCreatePage.tsx
+++ client/src/features/register-certificates/certificate/pages/CertificateCreatePage.tsx
@@ -1,14 +1,14 @@
 import { Card } from "../../../../shared/components/Card/Card";
 import DefaultLayout from "../../../../shared/layouts/DefaultLayout";
 import { SectionLayout } from "../../../../shared/layouts/SectionLayout";
-import { CreateCertificateForm } from "../components/CreateCertificateForm";
+import { CertificateForm } from "../components/CertificateForm";
 
 export const CertificateCreatePage = () => {
   return (
     <DefaultLayout>
       <SectionLayout title="Certificados médicos" subtitle="Registro de certificados">
         <Card headerCard="Registro">
-          <CreateCertificateForm />
+          <CertificateForm />
         </Card>
       </SectionLayout>
     </DefaultLayout>
