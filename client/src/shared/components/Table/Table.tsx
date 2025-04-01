7311ef2da6e4235dc026217a6180886de0f8bbbd
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-31 21:01:42 -0500

    feat(Table): add support for group headers in table component

--- client/src/shared/components/Table/Table.tsx
+++ client/src/shared/components/Table/Table.tsx
@@ -1,11 +1,17 @@
 import type { DataCollectionType } from "../../types/DataCollectionType";
 import type { DataResponseFromAPI } from "../../types/DataResponse";
 
+type GroupHeaderType = {
+  title: string;
+  colSpan: number;
+};
+
 interface Props {
   headersTable: Array<string>;
   response: DataResponseFromAPI<DataCollectionType> | Array<DataCollectionType>;
   children: React.ReactNode;
   hasButtonActions?: boolean;
+  groupHeadersTable?: Array<GroupHeaderType>;
 }
 
 export const Table: React.FC<Props> = ({
@@ -13,15 +19,42 @@ export const Table: React.FC<Props> = ({
   response,
   hasButtonActions = true,
   children,
+  groupHeadersTable,
 }) => {
   const hasData = Array.isArray(response) ? response.length > 0 : response?.data?.length > 0;
+  const isValidGroupHeader = groupHeadersTable && groupHeadersTable.length > 0;
 
-  return !hasData ? (
-    <div>No hay datos para mostrar</div>
-  ) : (
+  if (!hasData)
+    return (
+      <div className="text-paragraph-semibold pt-32">
+        No hay datos para mostrar{" "}
+        {groupHeadersTable && "ó no buscó por el campo correcto (solo código de certificado)"}
+      </div>
+    );
+
+  return (
     <div className="overflow-x-auto bg-shades-light">
       <table className="w-full">
         <thead className="text-center bg-neutral-50">
+          {isValidGroupHeader && (
+            <tr className="border-b border-neutral-100">
+              {groupHeadersTable.map((groupHead, index) => (
+                <th
+                  key={index}
+                  colSpan={groupHead.colSpan || 1}
+                  className={`p-4 text-paragraph-bold ${
+                    isValidGroupHeader &&
+                    groupHeadersTable.length > 1 &&
+                    index === 0 &&
+                    "border-r border-neutral-200"
+                  }`}
+                >
+                  {groupHead.title}
+                </th>
+              ))}
+            </tr>
+          )}
+
           <tr className="border-b border-neutral-100">
             {headersTable.map((header, index) => (
               <th key={index} className="p-4 text-paragraph-semibold">
