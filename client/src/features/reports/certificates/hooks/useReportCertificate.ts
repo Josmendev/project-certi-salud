091b9d59603b4473265fe16ad25785d936f9c193
CTZDev <carlosteranzavaleta@hotmail.com>
2025-03-31 21:02:43 -0500

    feat(hooks): add useReportCertificate hook for managing certificate reports

---
+++ client/src/features/reports/certificates/hooks/useReportCertificate.ts
@@ -0,0 +1,47 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import { BASE_ROUTES, initialListOfResponseAPI } from "../../../../shared/utils/constants";
+import { getAllCertificatesInReports } from "../repositories/reportsRepository";
+import { REPORT_CERTIFICATE_ROUTES } from "../utils/constants";
+import { useDeleteCertificate } from "./useDeleteCertificate";
+
+export const useReportCertificate = ({
+  currentPage,
+  searchQuery = "",
+}: {
+  currentPage: number;
+  searchQuery: string;
+}) => {
+  const navigate = useNavigate();
+  const MAIN_REPORTS_ROUTE = `/${BASE_ROUTES.PRIVATE.REPORTS}/${REPORT_CERTIFICATE_ROUTES.REPORT}`;
+  const certificateQuery = ["reports-certificates", currentPage, searchQuery];
+
+  const {
+    data = initialListOfResponseAPI,
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+  } = useQuery({
+    queryKey: certificateQuery,
+    queryFn: () => getAllCertificatesInReports({ page: currentPage, query: searchQuery }),
+    placeholderData: keepPreviousData,
+    staleTime: 10_000,
+  });
+
+  const handleDeleteCertificateMutation = useDeleteCertificate({
+    queryKey: certificateQuery,
+    onSuccess: (newPage) => {
+      navigate(`${MAIN_REPORTS_ROUTE}?page=${newPage}`);
+    },
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    handleDeleteCertificateMutation,
+  };
+};
