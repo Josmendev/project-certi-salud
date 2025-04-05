8c60221e063c7fe1c914e48f7ad55944478002c3
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-05 18:27:08 -0500

    feat(TypeCertificateRoutes): update navigation to redirect to the initial certificate type route

--- client/src/features/register-certificates/type-certificate/routes/TypeCertificateRoutes.tsx
+++ client/src/features/register-certificates/type-certificate/routes/TypeCertificateRoutes.tsx
@@ -1,14 +1,20 @@
 import { Navigate, Route, Routes } from "react-router";
-import { CRUD_ROUTES } from "../../../../shared/utils/constants";
+import {
+  BASE_ROUTES,
+  CRUD_ROUTES,
+  REGISTER_CERTIFICATE_ROUTES,
+} from "../../../../shared/utils/constants";
 import { CertificateTypeListPage } from "../pages/CertificateTypeListPage";
 
 export const TypeCertificateRoutes: React.FC = () => {
+  const ROUTE_INITIAL = `/${BASE_ROUTES.PRIVATE.REGISTER_CERTIFICATE}/${REGISTER_CERTIFICATE_ROUTES.TYPE_CERTIFICATES}`;
+
   return (
     <>
       <Routes>
         <Route index element={<CertificateTypeListPage />} />
         <Route path={CRUD_ROUTES.LIST} element={<CertificateTypeListPage />} />
-        <Route path="*" element={<Navigate to="." replace />} />
+        <Route path="*" element={<Navigate to={ROUTE_INITIAL} replace />} />
       </Routes>
     </>
   );
