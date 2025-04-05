797c96a641ca271a3d534ae14f2a9e223b5a4fc6
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-05 18:24:42 -0500

    fix(routes): update navigation to redirect to the correct initial role route

--- client/src/features/admin-users/roles/routes/RolesRoutes.tsx
+++ client/src/features/admin-users/roles/routes/RolesRoutes.tsx
@@ -1,15 +1,16 @@
 import type React from "react";
 import { Navigate, Route, Routes } from "react-router";
-import { CRUD_ROUTES } from "../../../../shared/utils/constants";
+import { ADMIN_USERS_ROUTES, BASE_ROUTES, CRUD_ROUTES } from "../../../../shared/utils/constants";
 import { RoleListPage } from "../pages/RoleListPage";
 
 export const RoleRoutes: React.FC = () => {
+  const ROUTE_INITIAL = `/${BASE_ROUTES.PRIVATE.ADMIN}/${ADMIN_USERS_ROUTES.ROLES}`;
   return (
     <>
       <Routes>
         <Route index element={<RoleListPage />} />
         <Route path={CRUD_ROUTES.LIST} element={<RoleListPage />} />
-        <Route path="*" element={<Navigate to="." replace />} />
+        <Route path="*" element={<Navigate to={ROUTE_INITIAL} replace />} />
       </Routes>
     </>
   );
