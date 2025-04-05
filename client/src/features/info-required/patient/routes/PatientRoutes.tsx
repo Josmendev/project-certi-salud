6b8f841c23e24e1dcef13cd7b372558d19346595
CTZDev <carlosteranzavaleta@hotmail.com>
2025-04-05 18:26:55 -0500

    feat(PatientRoutes): update navigation to redirect to the initial patient route

--- client/src/features/info-required/patient/routes/PatientRoutes.tsx
+++ client/src/features/info-required/patient/routes/PatientRoutes.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from "react-router";
-import { CRUD_ROUTES } from "../../../../shared/utils/constants";
+import { BASE_ROUTES, CRUD_ROUTES, INFO_REQUIRED_ROUTES } from "../../../../shared/utils/constants";
 import { PatientListPage } from "../pages/PatientListPage";
 import { PatientUpsertPage } from "../pages/PatientUpsertPage";
 
 export const PatientRoutes: React.FC = () => {
+  const ROUTE_INITIAL = `/${BASE_ROUTES.PRIVATE.INFO_REQUIRED}/${INFO_REQUIRED_ROUTES.PATIENTS}`;
+
   return (
     <>
       <Routes>
@@ -11,7 +13,7 @@ export const PatientRoutes: React.FC = () => {
         <Route path={CRUD_ROUTES.LIST} element={<PatientListPage />} />
         <Route path={CRUD_ROUTES.ADD} element={<PatientUpsertPage />} />
         <Route path={CRUD_ROUTES.EDIT} element={<PatientUpsertPage />} />
-        <Route path="*" element={<Navigate to="." replace />} />
+        <Route path="*" element={<Navigate to={ROUTE_INITIAL} replace />} />
       </Routes>
     </>
   );
